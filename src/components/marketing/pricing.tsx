ed6abb37a05703dab39b75c803872e4ddf7e4e73
versartist <shafi258011@gmail.com>
2025-05-06 12:31:25 +0400

    card

--- src/components/marketing/pricing.tsx
+++ src/components/marketing/pricing.tsx
@@ -1,187 +1,66 @@
 "use client";
+import {Card, CardHeader, CardBody, CardFooter, Divider, Link, Image} from "@heroui/react";
 
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { PLANS } from "@/constants";
-import { cn } from "@/functions";
-import { AnimatePresence, motion } from "framer-motion";
-import { CheckIcon } from "lucide-react";
-import Link from "next/link";
-import Container from "../global/container";
-import { Button } from "../ui/button";
-import NumberTicker from "../ui/number-ticker";
+import Container from "../global/container";import { Button } from "../ui/button";
 import { SectionBadge } from "../ui/section-bade";
 
 type Plan = "monthly" | "yearly";
+const cardData = [
+    { title: "Computer Vision & ML", description: "Wisepl provides professionally managed individuals who work on teams , are well thought out and trained in Data Annotation" },
+    { title: "Video Annotation", description: "We can annotate the any type of video using the latest technology and tools helping to build the computer vision model work with highest quality." },
+    { title: "Audio Annotation", description: "Our highly trained workforce can able to categorize the topic and determine emotion of peoples , also identify an important event in a snippet of audio." },
+    { title: "AI in Retails", description: "To make accurate, data-driven decision in retail, Wisepl use Services like Images and Videos annotation using bounding boxes or 2D and 3D cuboids." },
+    { title: "Medical Annotation", description: "We offer cost-effective Medical data annotation service like Semantic Segmentation and Polygon annotation with highest accuracy in diseases and organs diagnosis." },
+    { title: "Data Research for Universties & Companies", description: "Wisepl offer cutting-edge research and collaborations with universities other institutuons to achieve better business benefit." },
+    { title: "Logistics Annotation", description: "Using various annotation techniques.We help to identify Bar codes, QR codes for Supply chain and logistics Industries for better traking of Goods and courier" },
+    { title: "Sports & Games Annotation", description: "Using various annotation techniques. We analyze images and videos to deliver high quality training data for Sports & games Industrries" },
+    { title: "Web Development", description: "Wisepl has Professional team of Developers, which have good knowledge in Latest Technologies, We provide efficient and affordable applications" },
+    { title: "Insurance", description: "We help insurance Companies to Analyzing the insurance applications for claims, identifying risk, and reviewing Medical records" },
+    { title: "Branding", description: "Wisepl can cater to any of your design needs. From Logo designing to Brochure designing and Product catelog for your Business." },
+    { title: "Online Promotion", description: "Wisepl are here to helps to grow your business through perfect solutions." },
+    { title: "Business process outsourcing (BPO)", description: "Wisepl Bpo is a leading outsourcing service provider in South India and offers the consistent and reliable support services to all the clients." },
+    { title: "Software Design & Development", description: "Wisepl supplies and supports a broad range of IT application solutions and services in Best of quality with cost effective plan" },
+    { title: "Autonomus Vehicles", description: "Our project management team are expert in creating custom datasets for Autonomous vehicles and also provide annotation services , We detect obstacles in roads, distance calculation for lidar and radar images from obstecles" },
+    { title: "Agricultural Annotation", description: "For monitoring crop health, soil and field and also detection in growth progress,ripeness and unwanted plants and weeds In Agriculture. We provide various Services such as bounding box, polygon annotation, semantic segmentation, cuboid annotation, key points, and polylines." }
+  ];
 
 const Pricing = () => {
     return (
-        <div className="flex flex-col items-center justify-center py-12 md:py-16 lg:py-24  w-full relative">
+        <div className="flex flex-col items-center justify-center py-12 md:py-16 lg:py-24  w-full relative mb-15">
             <Container>
                 <div className="flex flex-col items-center text-center max-w-xl mx-auto">
-                    <SectionBadge title="Choose your plan" />
+                    <SectionBadge title="Services" />
                     <h2 className="text-2xl md:text-4xl lg:text-5xl font-heading font-medium !leading-snug mt-6">
-                        Simple and transparent pricing
+                        Services we provide
                     </h2>
-                    <p className="text-base md:text-lg text-center text-accent-foreground/80 mt-6">
-                        Choose the plan that suits your needs. No hidden fees, no surprises.
-                    </p>
+                    
                 </div>
+                <p className="text-base md:text-lg text-center text-accent-foreground/80 mt-6">
+                    Wisepl is providing all leading types of data annotation service used as data training in machine learning and deep learning. It offers Bounding Boxes, Semantic Segmentation, 3D Point Cloud Annotation and 3D Cuboid Annotation for fields like healthcare, autonomous driving or drone falying, retail, security surveillance and agriculture. Wisepl works with scalable solution, available at turnaround time and cost-effective pricing for clients across the globe.
+                   <br /><br /><br /><br /> </p>
             </Container>
-            <div className="mt-8 w-full relative flex flex-col items-center justify-center">
-                <div className="absolute hidden lg:block top-1/2 right-2/3 translate-x-1/4 -translate-y-1/2 w-96 h-96 bg-primary/15 blur-[10rem] -z-10"></div>
-                <div className="absolute hidden lg:block top-1/2 left-2/3 -translate-x-1/4 -translate-y-1/2 w-96 h-96 bg-violet-500/15 blur-[10rem] -z-10"></div>
-                <Container>
-                    <Tabs defaultValue="monthly" className="w-full flex flex-col items-center justify-center">
-                        <TabsList>
-                            <TabsTrigger value="monthly">
-                                Monthly
-                            </TabsTrigger>
-                            <TabsTrigger value="yearly">
-                                Yearly
-                            </TabsTrigger>
-                        </TabsList>
-                        <TabsContent value="monthly">
-                            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full mt-14">
-                                {PLANS.map((plan, index) => (
-                                    <Plan
-                                        key={index}
-                                        index={index}
-                                        {...plan}
-                                        plan="monthly"
-                                    />
-                                ))}
-                            </div>
-                        </TabsContent>
-                        <TabsContent value="yearly">
-                            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full mt-14">
-                                {PLANS.map((plan, index) => (
-                                    <Plan
-                                        key={index}
-                                        index={index}
-                                        {...plan}
-                                        plan="yearly"
-                                    />
-                                ))}
-                            </div>
-                        </TabsContent>
-                    </Tabs>
-                </Container>
+            <div>
+            
+
+<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 p-4 c pt-15">
+  {cardData.map((card, index) => (
+    <Card key={index} className=" max-w-[250px] bg-[#09090B] border border-[#31264d]">
+        <h2 className="px-6 py-5 font-bold"> {card.title}</h2>
+      <Divider />
+      <CardBody>
+        <p className="px-3 pb-10">{card.description}</p>
+      </CardBody>
+    </Card>
+  ))}
+</div>
+
+  
+
             </div>
+   
         </div>
     )
 };
 
-const Plan = ({
-    id,
-    title,
-    desc,
-    monthlyPrice,
-    yearlyPrice,
-    buttonText,
-    features,
-    index,
-    plan,
-}: {
-    id: string;
-    title: string;
-    desc: string;
-    monthlyPrice: number;
-    yearlyPrice: number;
-    buttonText: string;
-    features: string[];
-    index: number;
-    plan: Plan;
-}) => {
-
-    const getDisplayedPrice = (plan: string, monthlyPrice: number, yearlyPrice: number) => {
-        if (plan === "monthly") {
-            return monthlyPrice === 0 ? 0 : monthlyPrice;
-        } else if (plan === "yearly") {
-            const discountedPrice = Math.round((yearlyPrice * 0.8) / 12);
-            return yearlyPrice === 0 ? 0 : discountedPrice;
-        }
-        return 0;
-    };
-
-    const displayedPrice = getDisplayedPrice(plan, monthlyPrice, yearlyPrice);
-
-    return (
-        <div key={index} className="w-full relative flex flex-col saturate-150 rounded-2xl">
-
-            <div
-                className={cn(
-                    "flex flex-col size-full border rounded-2xl relative p-3 [background-image:linear-gradient(345deg,rgba(255,255,255,0.01)_0%,rgba(255,255,255,0.03)_100%)]",
-                    id === "pro" ? "border-primary/80" : "border-border/60",
-                )}
-            >
-                {id === "pro" && (
-                    <div className="max-w-fit min-w-min inline-flex items-center whitespace-nowrap px-1 h-7 rounded-full bg-gradient-to-r from-primary to-violet-500 absolute -top-3 left-1/2 -translate-x-1/2 select-none">
-                        <span className="flex-1 text-sm px-2 font-medium bg-gradient-to-r from-foreground to-foreground/80 bg-clip-text text-transparent bg-[length:250%_100%] animate-background-shine">
-                            Most Popular
-                        </span>
-                    </div>
-                )}
-                <div className="flex flex-col p-3 w-full">
-                    <h2 className="text-xl font-medium">
-                        {title}
-                    </h2>
-                    <p className="text-sm mt-2 text-muted-foreground break-words">
-                        {desc}
-                    </p>
-                </div>
-                <hr className="shrink-0 border-none w-full h-px bg-border" role="separator" />
-                <div className="relative flex flex-col flex-1 align-top w-full p-3 h-full break-words text-left gap-4">
-                    <div className="flex items-end gap-2">
-                        <div className="flex items-end gap-1 w-40">
-                            <span className="text-3xl md:text-4xl font-bold">
-                                ${displayedPrice === 0 ? 0 : <NumberTicker value={displayedPrice} />}
-                            </span>
-                            {/* In here 120 * 0.8 = 96 and /12 to get monthly price */}
-                            <span className="text-lg text-muted-foreground font-medium font-headin">
-                                per {plan === "monthly" ? "month" : "month"}
-                            </span>
-                        </div>
-                        <AnimatePresence>
-                            {(id !== "free" && plan === "yearly") && (
-                                <motion.span
-                                    initial={{ opacity: 0, scale: 0 }}
-                                    animate={{ opacity: 1, scale: 1 }}
-                                    exit={{ opacity: 0, scale: 0 }}
-                                    transition={{ duration: 0.2 }}
-                                    aria-hidden="true"
-                                    className="text-xs px-2 py-0.5 rounded mb-1 text-foreground bg-primary font-medium"
-                                >
-                                    -20%
-                                </motion.span>
-                            )}
-                        </AnimatePresence>
-                    </div>
-                    <ul className="flex flex-col gap-2">
-                        {features.map((feature, index) => (
-                            <li key={index} className="flex items-center gap-2">
-                                <CheckIcon aria-hidden="true" className="w-5 h-5 text-primary" />
-                                <p className="text-sm md:text-base text-muted-foreground">
-                                    {feature}
-                                </p>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-                <div className="p-3 mt- h-auto flex w-full items-center">
-                    <Button
-                        asChild
-                        variant={id === "pro" ? "default" : "tertiary"}
-                        className="w-full hover:scale-100 hover:translate-y-0 shadow-none"
-                    >
-
-                        <Link href={""}>
-                            {buttonText}
-                        </Link>
-                    </Button>
-                </div>
-            </div>
-        </div>
-    )
-};
 
 export default Pricing
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import {heroui} from '@heroui/theme';
 import type { Config } from "tailwindcss"
 
 const svgToDataUri = require("mini-svg-data-uri");
@@ -10,10 +11,11 @@ const {
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   prefix: "",
   theme: {
@@ -123,33 +125,21 @@ const config = {
       },
     },
   },
-  plugins: [
-    addVariablesForColors,
-    require("tailwindcss-animate"),
-    require("tailwind-scrollbar-hide"),
-    function ({ matchUtilities, theme }: any) {
+  plugins: [addVariablesForColors,require("tailwindcss-animate"),require("tailwind-scrollbar-hide"),function ({ matchUtilities,theme }: any) {
       matchUtilities(
         {
           "bg-grid": (value: any) => ({
             backgroundImage: `url("${svgToDataUri(
               `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-            )}")`,
-          }),
-          "bg-grid-small": (value: any) => ({
+            )}")`,}),"bg-grid-small": (value: any) => ({
             backgroundImage: `url("${svgToDataUri(
               `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-            )}")`,
-          }),
-          "bg-dot": (value: any) => ({
+            )}")`,}),"bg-dot": (value: any) => ({
             backgroundImage: `url("${svgToDataUri(
               `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
-            )}")`,
-          }),
-        },
-        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+            )}")`,}),},{ values: flattenColorPalette(theme("backgroundColor")),type: "color" }
       );
-    },
-  ],
+    },heroui()],
 } satisfies Config;
 
 function addVariablesForColors({ addBase, theme }: any) {
